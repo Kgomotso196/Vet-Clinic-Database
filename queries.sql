ca486a429e9f96439e6cfad48b7e1447fc6d3e68
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-08-18 08:55:19 +0200

    Apply required changes

--- queries.sql
+++ queries.sql
@@ -22,33 +22,16 @@ SELECT species from animals;
 
 BEGIN;
 
-    UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-
-    UPDATE animals SET species = 'pokemon' WHERE species is null;
-
 SELECT species from animals;
 COMMIT;
 SELECT species from animals;
 
 BEGIN;
 
-    DELETE FROM animals;
-
-SELECT species from animals;
 ROLLBACK;
 SELECT species from animals;
 
 BEGIN;
-
-    DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-
-    SAVEPOINT SP1;
-
-    UPDATE animals SET weight_kg = weight_kg * -1;
-
-    ROLLBACK TO SP1:
-
-    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 
 COMMIT;
 SELECT species from animals;
